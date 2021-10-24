46b3586910f76d1a509f7a80cd14f3cd6ac5ff38
JNait <JekyllMNait@gmail.com>
2021-10-24 22:21:29 +0300

    Added support for console commands: -ti    time interval -lf    log file name
    Set the default value for the log recording time interval and file name.
    Refactoring to object oriented programming.

--- PyDispatcherL/PyDispatcherL.py
+++ PyDispatcherL/PyDispatcherL.py
@@ -4,50 +4,53 @@ import subprocess
 import time
 
 
-def add_note(fileL, message):
-    fileL.write("[" + str(datetime.datetime.now()) + "]: ")
-    fileL.write(str(message))
-    fileL.write("\n")
+class Dispatcher:
+    def __init__(self) -> None:
+        self.logger = Logger()
+
+    def start_procces(self) -> None:
+        print("Enter path to programe (tromble):")
+        commands = input().split("-")
+        process_path = commands.pop(0).strip()
+        for command in commands:
+            command_pair = command.strip().split(" ")
+            self._process_command(command_pair[0], command_pair[1])
+        procces = subprocess.Popen(process_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.logger.run()
+
+    def _process_command(self, command, parametr) -> None:
+        if command == "ti":
+            self.logger.polling_interval = float(parametr)
+        elif command == "lf":
+            self.logger.log_file_path = parametr
 
 
-def logging():
-    fileL = open("Testlog.txt", 'a')
-    targetProcess = psutil.Process().children()[0]
-    while (True):
-        print("!")
-        #add_note(fileL, "CPU")
-        #add_note(fileL, "RSS")
-        #add_note(fileL, "VMS")
-        #add_note(fileL, "NUM_HANDLE")
-        message = "|CPU: " + str(targetProcess.cpu_percent()) + "|RSS: " + str(targetProcess.memory_info()[0]) + "|VMS: " + str(targetProcess.memory_info()[1]) + "|N_H: " + str(targetProcess.num_handles())
+class Logger:
+    def __init__(self, polling_interval = 0.5, log_file_path = "logs.txt") -> None:
+        self.polling_interval = polling_interval
+        self.log_file_path = log_file_path
 
-        add_note(fileL, message)
-        time.sleep(0.1)
-    fileL.close()
+    def _add_note(self, message) -> None:
+        self.log_file.write("[" + str(datetime.datetime.now()) + "]")
+        self.log_file.write(str(message))
+        self.log_file.write("\n")
 
+    def run(self) -> None:
+        self.log_file = open(self.log_file_path, 'a')
+        targetProcess = psutil.Process().children()[0]
+        while (targetProcess.is_running()):
+            print("!")
+            #add_note(fileL, "CPU")
+            #add_note(fileL, "RSS")
+            #add_note(fileL, "VMS")
+            #add_note(fileL, "NUM_HANDLE")
+            message = "|CPU: " + str(targetProcess.cpu_percent()) + "|RSS: " + str(targetProcess.memory_info()[0]) + "|VMS: " + str(targetProcess.memory_info()[1]) + "|N_H: " + str(targetProcess.num_handles())
 
-def print_start_resurse_info_main():
-    print (psutil.cpu_percent()) #CUP в процентах
-    print (psutil.virtual_memory()) #Виртуальная память
-    print (dict(psutil.virtual_memory()._asdict())["total"])
-    print (psutil.virtual_memory().percent) #Виртуальная память в процентах
-    print (psutil.virtual_memory().available * 100 / psutil.virtual_memory().total) #Процент доступной виртуальной памяти (обратное)
-
-
-def print_resurse_info(target):
-    print(target.cpu_percent())
-    print(target.memory_full_info())
-    print(target.memory_percent())
-
-
-def start_procces():
-    print_start_resurse_info_main()
-
-    print("Enter path to programe (tromble):")
-    process_path = "notepad.exe"
-    procces = subprocess.Popen(process_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    logging()
+            self._add_note(message)
+            time.sleep(self.polling_interval)
+        self.log_file.close()
 
 
 if __name__ == '__main__':
-    start_procces()
+    dispatcher = Dispatcher()
+    dispatcher.start_procces()
